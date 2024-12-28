1fb7b011011880d854ffd4bc1fe4f11e2f176252
Mahek Singh <mahek.2002.singh@gmail.com>
2024-12-29 04:52:21 +0530

    adding push array

---
+++ pushArray.php
@@ -0,0 +1,6 @@
+<?php
+$lang = array("a" => "php", "b" => "java");
+array_push($lang, "laravel", "bluej");
+print_r($lang);
+
+// Defintion : The array_push() function returns the new number of elements in the array.
